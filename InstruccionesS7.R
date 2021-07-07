0af84a217a8080cd630c855ff494bbe2228f99a0
Daniela-GC98 <daniela.gcruz98@gmail.com>
2021-07-07 01:57:32 +0000

    Initial commit

---
+++ InstruccionesS7.R
@@ -0,0 +1,6 @@
+#Este es un push
+#Código para configurar desde terminal:
+#/cloud/project$ git config --global user.email "daniela.gcruz98@gmail.com"
+#/cloud/project$ git config --global user.name "Daniela-GC98"
+
+#Se seleccionan los archivos que se quieren subir desde ventana de Git
